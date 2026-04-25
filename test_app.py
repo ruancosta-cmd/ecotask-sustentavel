05e371c4a67969a3072fdb079e6f52e22c87c238
ruancosta-cmd <ruan.costa@sempreceub.com>
2026-04-25 20:12:08 -0300

    test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,9 @@
-import pytest
 from app import orientar_descarte, validar_cep
 
 def test_descarte():
     assert "azul" in orientar_descarte("papel")
 
 def test_api_viacep():
-    # Teste de integração
+    # Teste de integracao
     res = validar_cep("01001000")
     assert res is not None
