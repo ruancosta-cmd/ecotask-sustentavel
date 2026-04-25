773bf9b5b7c2865fdb3617be190b3ab4f2b7dac6
ruancosta-cmd <ruan.costa@sempreceub.com>
2026-04-25 18:58:28 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 from app import orientar_descarte, validar_cep
 
-def test_descarte():
+def test_fluxo_descarte():
     assert "Lixo azul" in orientar_descarte("papel")
 
-def test_viacep():
-    # Teste de integração real
+def test_integracao_viacep():
+    # Valida se a aplicação consegue falar com a API externa
     assert validar_cep("01001000") is not None
