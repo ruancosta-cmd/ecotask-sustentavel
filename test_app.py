a7b3db7840b249e0db0b50b8d04349dc8097e834
ruancosta-cmd <ruan.costa@sempreceub.com>
2026-04-25 19:29:14 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,10 @@
 from app import orientar_descarte, validar_cep
 
-def test_descarte():
-    assert "Lixo azul" in orientar_descarte("papel")
+def test_simples():
+    assert orientar_descarte("papel") == "Lixo azul"
 
-def test_viacep_integracao():
-    # Teste de integracao real
+def test_api_integracao():
+    # Teste de integracao real com a API ViaCEP
     resultado = validar_cep("01001000")
     assert resultado is not None
-    assert "Se" in resultado or "S" in resultado
+    assert "Praça da Sé" in resultado or "Praca da Se" in resultado
